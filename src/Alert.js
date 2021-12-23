d965765d22b236ed311394862e4887d12e005d40
Mohsen Hussein <mohsenhussen8@gmail.com>
2021-12-23 14:39:23 +0200

    add grocery style

---
+++ src/Alert.js
@@ -0,0 +1,11 @@
+import React , {useEffect} from 'react'
+
+const Alert = () => {
+    return (
+        <div>
+            Alert Component
+        </div>
+    )
+}
+
+export default Alert;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState , useEffect} from 'react';
+import List from './List';
+import Alert from './Alert';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: true, msg: '', type: '' });
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!name) {
+
+    } else if (name && isEditing) {
+
+    } else {
+      const newItem = {id: new Date().getTime().toString() , title: name};
+      setList([...list, newItem]);
+      setName('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className='form-control'>
+          <input
+            type="text"
+            className="grocery"
+            placeholder='e.g. EGG'
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
+          />
+          <button type='submit' className="submit-btn">
+            {isEditing? 'Edit' : 'Submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list}/>
+          <button className="clear-btn">
+            Clear Items
+          </button>
+        </div>
+      )}
+    </section>
   );
 }
 
